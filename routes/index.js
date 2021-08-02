f5b944943e706fb0914142f7f3569c79158cde66
krishnathakorkt <kthakor8445@conestogac.on.ca>
2021-08-02 17:58:27 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. 
+This all get page according to url
+*/
+router.get('/index', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Express' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Express' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Express' });
+});
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
